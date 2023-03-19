d7104649a5c05f0c30c40af60be90db10c0243ff
Ryan A.L. Schoop <93548953+RALSchoop@users.noreply.github.com>
2023-03-19 12:46:56 +0100

    Added global metrics evaluation script

--- global_metrics.py
+++ global_metrics.py
@@ -1,23 +1,21 @@
-"""Evaluate local evaluation metrics."""
+"""Evaluation using global evaluation metrics."""
 
-from typing import Tuple
 import torch
 import numpy as np
 import deepus
 import matplotlib.pyplot as plt
 import torch.utils.data
 import torchvision
-from matplotlib.patches import Ellipse
 import evaluation as eva
 from os.path import join
 
 # Global initializations.
 torch.manual_seed(seed := 1)
 
-# Specify your path to the root data directory for ROI based evaluation.
-# This should be of the form: r'...\CIRS040GSE' or r'.../CIRS040GSE',
+# Specify your path to the root data directory for training and testing.
+# This should be of the form: r'...\CIRS073_RUMC' or r'.../CIRS073_RUMC',
 # depending on your os's seperator.
-root_data_dir = r'D:\Files\CWI Winter Spring 2022\Data\DeepUS\TrainingData\CIRS040GSE'
+root_data_dir = r'D:\Files\CWI Winter Spring 2022\Data\DeepUS\TrainingData\CIRS073_RUMC'
 
 # Initialize dataset.
 deepus_dataset = deepus.UltrasoundDataset(
@@ -28,15 +26,8 @@ deepus_dataset = deepus.UltrasoundDataset(
             [torchvision.transforms.ToTensor()]))
 h_data = deepus_dataset.load_header()
 
-# Sample choice
-# Indices 1 to 5 contain hypoechoic regions, indices 11 to 15 have
-# circular regions of +3 and +6 dB, and indices 16 to 20 have circular
-# regions of -3 and -6 dB from a region with 'low' attentuation coefficient
-# of 0.70 dB/(cm MHz).
-# Indices 21 to 25, 31 to 35, and 36 to 40 contain the same type of
-# regions respectively, but from a region with 'high' attenuation
-# coefficient of 0.95 dB/(cm MHz).
-idx = 22
+# Select a sample from the test set.
+idx = 10
 
 # Load data of specified sample.
 input, target = deepus_dataset[idx]
@@ -67,23 +58,9 @@ msd_paths = [join(runs_root, 'gpu_s_runs', 'deepus_experiment_s1',
 model_outputs = [eva.model_output(model, msd_path, input)
                  for msd_path in msd_paths]
 
-# Specify circle and ring with common center for evaluation of local metrics.
-# Specify center as an ij coordinate tuple, that is axial coordinate first.
-# Given in pixel coordinates.
-c_ij = (597, 62)
-# Radius of circle corresponding to 'contrast' region for evaluation metrics.
-# Given in mm.
-r1 = 1.3
-# Radii of ring corresponding to 'background' region for evaluation metrics.
-# Given in mm.
-r2 = (2.9, 4.0)
-
 # Function for display.
 def display_img(img: torch.Tensor,
                 title: str = '',
-                c_ij: Tuple[float, float] = None,
-                r1: float = None,
-                r2: Tuple[float, float] = None,
                 physical_units = True,
                 pdeltaz: float = 2.4640e-2,
                 pdeltax: float = 1.9530e-1) -> None:
@@ -101,19 +78,8 @@ def display_img(img: torch.Tensor,
         axs.set_yticks(np.round(np.array([0, 5, 10, 15, 20, 25, 30, 35])
                                 / pdeltaz), ['0', '5', '10', '15', '20',
                                              '25', '30', '35'])
-    if c_ij is not None:
-        c_xy = (c_ij[1], c_ij[0])
-        if r1 is not None:
-            axs.add_patch(Ellipse(c_xy, 2*r1/pdeltax, 2*r1/pdeltaz,
-                                facecolor='none', edgecolor='g'))
-        if r2 is not None:
-            axs.add_patch(Ellipse(c_xy, 2*r2[0]/pdeltax, 2*r2[0]/pdeltaz,
-                                facecolor='none', edgecolor='r'))
-            axs.add_patch(Ellipse(c_xy, 2*r2[1]/pdeltax, 2*r2[1]/pdeltaz,
-                                facecolor='none', edgecolor='r'))
-        
+
 # Set flag if you want to see all images.
-# Use display_img() c_xy, r1 and r2 arguments to check the desired ROI.
 display = True
 if display:
     display_img(input_img, 'Input')
@@ -121,41 +87,40 @@ if display:
     for i, model_img in enumerate(model_outputs):
         display_img(model_img, f'Model Output {i}')
 
-# Evaluation metrics: contrast to noise ratio (CNR)
-input_cnr = eva.cnr(input_img, c_ij=c_ij, r1=r1, r2=r2)
-target_cnr = eva.cnr(target, c_ij=c_ij, r1=r1, r2=r2)
-model_cnrs = [eva.cnr(model_img, c_ij=c_ij, r1=r1, r2=r2)
-              for model_img in model_outputs]
+# Evaluation metric: peak signal to noise ratio (PSNR)
+input_psnr = eva.psnr(input_img, target)
+model_psnrs = [eva.psnr(model_img, target) for model_img in model_outputs]
 
-# Evaluation metrics: contrast ratio (CR)
-input_cr = eva.cr(input_img, c_ij=c_ij, r1=r1, r2=r2)
-target_cr = eva.cr(target, c_ij=c_ij, r1=r1, r2=r2)
-model_crs = [eva.cr(model_img, c_ij=c_ij, r1=r1, r2=r2)
-             for model_img in model_outputs]
+# Evaluation metric: l1 loss
+input_l1loss = eva.l1loss(input_img, target)
+model_l1losses = [eva.l1loss(model_img, target) for model_img in model_outputs]
 
-# Evaluation metrics: generalized contrast to noise ratio (gCNR)
-input_gcnr = eva.gcnr(input_img, c_ij=c_ij, r1=r1, r2=r2)
-target_gcnr = eva.gcnr(target, c_ij=c_ij, r1=r1, r2=r2)
-model_gcnrs = [eva.gcnr(model_img, c_ij=c_ij, r1=r1, r2=r2)
-               for model_img in model_outputs]
+# Evaluation metric: l2 loss
+input_l2loss = eva.l2loss(input_img, target)
+model_l2losses = [eva.l2loss(model_img, target) for model_img in model_outputs]
+
+# Evalution metric: normalized cross correlation (NCC)
+input_ncc = eva.ncc(input_img, target)
+model_nccs = [eva.ncc(model_img, target) for model_img in model_outputs]
 
 # Now you can save the evaluation metrics' results to your liking.
-# Or add to this script to use them in whatever way you'd like.
+# Or modify this script to use them in whatever way you'd like.
 
-print('CNR:')
-print(f'Input CNR: {input_cnr}')
-print(f'Target CNR: {target_cnr}')
-print(f'Model CNRs: {model_cnrs}')
+print('PSNR:')
+print(f'Input PSNR: {input_psnr}')
+print(f'Model PSNRs: {model_psnrs}')
 print() # Just for '\n'.
-print('CR:')
-print(f'Input CR: {input_cr}')
-print(f'Target CR: {target_cr}')
-print(f'Model CRs: {model_crs}')
-print() # Just for '\n'.
-print('gCNR:')
-print(f'Input gCNR: {input_gcnr}')
-print(f'Target gCNR: {target_gcnr}')
-print(f'Model gCNRs: {model_gcnrs}')
+print('l1 loss:')
+print(f'Input l1 loss: {input_l1loss}')
+print(f'Model l1 losses: {model_l1losses}')
+print()
+print('l2 loss:')
+print(f'Input l2 loss: {input_l2loss}')
+print(f'Model l2 losses: {model_l2losses}')
+print()
+print('NCC:')
+print(f'Input NCC: {input_ncc}')
+print(f'Model NCCs: {model_nccs}')
 
 if display:
     # Locks further code execution so show last.
